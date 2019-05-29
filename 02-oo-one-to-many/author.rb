fe38872a8e7c5c70a2f7d33627123936f1a724d2
Maxcell Wilson <maxcell.wilson@gmail.com>
2019-05-29 15:54:52 -0400

    Update code to have comments

--- 02-oo-one-to-many/author.rb
+++ 02-oo-one-to-many/author.rb
@@ -4,33 +4,45 @@ require 'pry'
 class Author
 
   attr_accessor :name, :age, :education
-  # attr_reader :name, :age, :education
-  # attr_writer :name, :age, :education
+  # attr_reader :name, :age, :education #=> Builds methods so that we can read instance variables with the same name
+  # attr_writer :name, :age, :education #=> Builds methods so that we can write to instance variables with the same name
 
-  # Author.new
-  def initialize(name, age, education=nil)
+  # initialize is the method we can write to overwrite the default .new method
+  # Author.new("Prince", 1, "College")
+  def initialize(name, age, education=nil) # Make sure to put default variables at the end
     @name = name
     @age = age
     @education = education
   end
 
+  # This method will create new instances of books for us, will require a title, pages, and genre to be passed in
   def write_book(title, pages, genre)
     # END RESULT: Is to add a new book to the @books for this author
     Book.new(pages, title, genre, self)
   end
 
+  # This method is what we will use to build the relationship "has_many"
+  # An Author **HAS MANY** books. We're making sure that we access all books
+  # ever created and ** SELECTING ** the ones that belong to the particular author
+  # that called it
   def books
     Book.all.select do |book|
       book.author.name == self.name
       # book.author.name == @name
+      # A BETTER WAY TO CHECK IF THEY ARE THE SAME AUTHOR
+      # vvvvvvvvvvvvvvv
+      # book.author == self
     end
   end
 
+  # Attribute Writer Method, we'll use a macro to take care of this
+  # **REMEMBER TO NOT HAVE THIS METHOD AND THE MACRO ABOVE**
   # def name=(new_name)
   #   @name = new_name
   # end
 
-
+  # Attribute Reader Method, we'll use a macro to take care of this
+  # **REMEMBER TO NOT HAVE THIS METHOD AND THE MACRO ABOVE**
   # def name # instance
   #   @name
   # end
--- 02-oo-one-to-many/book.rb
+++ 02-oo-one-to-many/book.rb
@@ -5,6 +5,8 @@ class Book
 
   attr_reader :pages, :title, :genre, :author
 
+  # No matter how a book gets created, whether through #write_book in the Author
+  # or if they call Book.new, we will be able to keep track of the book in the **class** variable
   @@all = []
 
   def initialize(pages, title, genre, author)
@@ -13,10 +15,14 @@ class Book
     @genre = genre
     @author = author
 
+    # Each time we create a new book, it is going to be added to this class variable
     @@all << self
   end
 
 
+  # In order to have access to this class variable inside of other files, we will want
+  # to have a class method
+  # Remember a class method is called with the name of the class and then the method name
   def self.all
     @@all
   end
